fc36a24d486d229abcecc2eb44aa2a0412dfa718
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-07-13 15:13:58 +0600

    Added mode of css lazyloading

--- webpackConfigFactory.js
+++ webpackConfigFactory.js
@@ -7,13 +7,16 @@ var _ = require('lodash'),
     ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = function (isDevelopment, config) {
+    var extractCss = config.extractCss !== undefined ? config.extractCss : true;
+
     return {
         context: __dirname + '/../../',
         watch: isDevelopment,
         devtool: isDevelopment ? 'cheap-inline-source-map' : null,
 
         output: {
-            filename: isDevelopment ? '[name].js' : '[name]-[hash].js'
+            filename: isDevelopment ? '[name].js' : '[name]-[hash].js',
+            publicPath: config.publicDir
         },
 
         resolve: {
@@ -27,38 +30,50 @@ module.exports = function (isDevelopment, config) {
 
         module: {
 
-            loaders: [{
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /(node_modules|bower_components)/,
-                query: {
-                    presets: ['es2015']
-                }
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract(isDevelopment ? 'css' : 'css?minimize')
-            }, {
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract(isDevelopment ? 'css!less' : 'css?minimize!less')
-            }, {
-                test: /\.(ico|png|jpg|gif|svg|ttf|otf|eot|woff|woff2)/,
-                include: new RegExp(config.frontendDir),
-                loader: 'file?name=[1]&regExp=' + config.frontendDir + '(.*)'
-            }, {
-                test: /\.(ico|png|jpg|gif|svg|ttf|otf|eot|woff|woff2)/,
-                include: /\/(bower_components|node_modules)\//,
-                loader: 'file?name=[2]&regExp=/(bower_components|node_modules)/(.*)'
-            }]
+            loaders: _.concat(
+                [{
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /(node_modules|bower_components)/,
+                    query: {
+                        presets: ['es2015']
+                    }
+                }],
 
+                extractCss ? [{
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract(isDevelopment ? 'css' : 'css?minimize')
+                }, {
+                    test: /\.less$/,
+                    loader: ExtractTextPlugin.extract(isDevelopment ? 'css!less' : 'css?minimize!less')
+                }] : [{
+                    test: /\.css$/,
+                    loader: isDevelopment ? 'style!css' : 'style!css?minimize'
+                }, {
+                    test: /\.less$/,
+                    loader: isDevelopment ? 'style!css!less' : 'style!css?minimize!less'
+                }],
+
+                [{
+                    test: /\.(png|jpg|gif|svg|ttf|otf|eot|woff|woff2)/,
+                    include: new RegExp(config.frontendDir),
+                    loader: 'file?name=[1]&regExp=' + config.frontendDir + '(.*)'
+                }, {
+                    test: /\.(png|jpg|gif|svg|ttf|otf|eot|woff|woff2)/,
+                    include: /\/(bower_components|node_modules)\//,
+                    loader: 'file?name=[2]&regExp=/(bower_components|node_modules)/(.*)'
+                }]
+            )
         },
 
         plugins: _.concat([
             new webpack.NoErrorsPlugin(),
-            new ExtractTextPlugin(isDevelopment ? '[name].css' : '[name]-[hash].css', {allChunks: true}),
             new ManifestPlugin({basePath: config.publicDir}),
             new webpack.ProvidePlugin(config.provides)
         ], isDevelopment ? [] : [
             new webpack.optimize.UglifyJsPlugin()
+        ], !extractCss ? [] : [
+            new ExtractTextPlugin(isDevelopment ? '[name].css' : '[name]-[hash].css', {allChunks: true})
         ])
     };
 };
